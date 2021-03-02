082c7ca8733e4c02f572f3852dfa7a880cb6a53f
KevinZhang <zhang_kun_lin@outlook.com>
2021-03-03 01:19:07 +0800

    change push.sh

--- docs/push.sh
+++ docs/push.sh
@@ -1,17 +1,43 @@
 echo 'make dictionary ...'
 book sm -t 'leetcode 刷题笔记'
 
+pwd=`pwd`
+path=`basename $pwd`
+
 echo 'gitbook build ...'
 cd ../
-gitbook build ToDoList ToDoList/docs
+gitbook build $path $path/docs
 
-cd ToDoList
+cd leetcode
 
 echo 'git add all ...'
 git add --all
 
+res=`echo $?`
+if [ "$res" != "0" ]
+then 
+    exit 1
+    echo '---->> error : git add failed'
+fi
+
 echo 'git commit ...'
-git commit -a -m "add md file"
+gitCommit='git commit -a -m "add md file"'
+
+res=`echo $?`
+if [ "$res" != "0" ]
+then
+    exit 1
+    echo '---->> error : git add failed'
+
+fi
 
 echo 'git push ...'
 git push
+
+res=`echo $?`
+if [ "$res" != "0" ]
+then
+    exit 1
+     echo '---->> error : git add failed'
+
+fi
